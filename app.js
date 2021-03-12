caa79463d1e44ab8a0e847779ca198b9970397ae
LeeChan <lee79102@hotmail.com>
2021-03-12 19:30:14 +0800

    feat: add user login feature vie passport

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const session = require('express-session')
 const exphbs = require('express-handlebars')
-const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
 const routes = require('./routes')
 require('./config/mongoose')
+
+const usePassport = require('./config/passport')
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -21,8 +22,11 @@ app.use(
     })
 )
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
+//body-parser built into Express.js
 app.use(methodOverride('_method'))
+
+usePassport(app)
 
 app.use(routes)
 
